0321961721e28ac17305a9a9e12168cf5f66b536
kaatjeskiddos <124719658+kaatjeskiddos@users.noreply.github.com>
2023-03-11 22:35:54 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
+	$vragen = $_POST['vragen'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, email, number, vragen) values(?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssis", $firstName, $lastName, $email, $number, $vragen);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
